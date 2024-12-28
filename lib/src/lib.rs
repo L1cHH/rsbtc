13f72a46b10f0ebc308e0c5563f0998634865f38
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2024-12-28 21:12:14 +0300

    Added hash function and also Dispay trait has been implemented

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -3,8 +3,10 @@ mod types;
 mod util;
 mod crypto;
 
+use serde::{Deserialize, Serialize};
 use uint::construct_uint;
 
 construct_uint! {
+    #[derive(Serialize, Deserialize)]
     pub struct U256(4);
 }
--- lib/src/sha256.rs
+++ lib/src/sha256.rs
@@ -1,1 +1,41 @@
-pub struct Hash;
+use crate::U256;
+use sha256::digest;
+use serde::Serialize;
+use std::fmt;
+use std::fmt::{Display, Formatter, write};
+
+#[derive(Serialize, Clone, Copy)]
+pub struct Hash(U256);
+
+impl Hash {
+    pub fn hash<T: serde::Serialize>(data: &T) -> Self {
+        let mut serialized: Vec<u8> = Vec::new();
+
+        if let Err(e) = ciborium::into_writer(
+            data,
+            &mut serialized
+        ) {
+            panic!(
+                "Failed to serialize data: {:?}. \
+                This should not happen",
+                e
+            );
+        }
+
+        let hash = digest(&serialized);
+        let hash_bytes = hex::decode(hash).unwrap();
+        let hash_slice = hash_bytes.as_slice();
+
+        Hash(U256::from_big_endian(hash_slice))
+    }
+
+    pub fn matches_target(&self, target: U256) -> bool {
+        self.0 <= target
+    }
+}
+
+impl Display for Hash {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:x}", self.0)
+    }
+}
